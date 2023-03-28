f15957fac3715c5d3cd030bd2a90da1fe7f97b44
Володимир Nagual <nahualli.ya@gmail.com>
2023-03-28 17:20:11 +0300

    Add <Block> model

---
+++ Models/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Thoujour.Models
+{
+    public class Block
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ThoughtId { get; set; }
+        public string? Title { get; set; }
+        public string? B64Img { get; set; }
+        public string? Text { get; set; }
+    }
+}
