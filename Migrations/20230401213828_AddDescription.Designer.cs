8f70e5117dc3d169b5ec3f40c40e6b00815eb9fc
Володимир Nagual <nahualli.ya@gmail.com>
2023-04-02 00:49:52 +0300

    Migration and some front-end

--- Migrations/20230401213828_AddDescription.Designer.cs
+++ Migrations/20230401213828_AddDescription.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Thoujour.Migrations
 {
     [DbContext(typeof(ThoughtsDb))]
-    partial class ThoughtsDbModelSnapshot : ModelSnapshot
+    [Migration("20230401213828_AddDescription")]
+    partial class AddDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
