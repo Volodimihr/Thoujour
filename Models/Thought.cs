d7d16770bd28c0e230d3199879c51ce081227aa3
Володимир Nagual <nahualli.ya@gmail.com>
2023-04-02 00:49:07 +0300

    Add Description to <THought>

--- Models/Thought.cs
+++ Models/Thought.cs
@@ -8,6 +8,7 @@ namespace Thoujour.Models
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
+        public string? Description { get; set; }
 
         public List<Block>? Blocks { get; set; }
     }
