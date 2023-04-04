32330d450a9d73c7ae236cf84acdc0ce8647ec4a
Володимир Nagual <nahualli.ya@gmail.com>
2023-04-04 03:50:21 +0300

    Create <Comment> model

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Thoujour.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("FK_Thought_002")]
+        public int ThoughtId { get; set; }
+        public string? UserName { get; set; }
+        [Required]
+        public string Text { get; set;}
+        [Required]
+        public DateTime Date { get; set; }
+
+        public Thought? Thought { get; set; }
+    }
+}
